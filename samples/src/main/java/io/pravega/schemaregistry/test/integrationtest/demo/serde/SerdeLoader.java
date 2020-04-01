4fcd5aea9dd042b1fa3e83af3227b3823b702d7d
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2020-03-31 22:25:42 -0700

    serde demo
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

---
+++ samples/src/main/java/io/pravega/schemaregistry/test/integrationtest/demo/serde/SerdeLoader.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.schemaregistry.test.integrationtest.demo.serde;
+
+import io.pravega.schemaregistry.serializers.PravegaDeserializer;
+import io.pravega.schemaregistry.serializers.PravegaSerializer;
+import lombok.SneakyThrows;
+
+import java.net.URL;
+import java.security.PrivilegedAction;
+
+import static java.security.AccessController.doPrivileged;
+
+public class SerdeLoader {
+    public static PravegaSerializer getSerializer(String className, URL url) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        final PrivilegedAction<PravegaSerializer> action = new PrivilegedAction<PravegaSerializer>() {
+            @Override
+            @SneakyThrows
+            @SuppressWarnings("unchecked")
+            public PravegaSerializer run() {
+                    Class<?> aClass;
+                    // Load the class.
+                    URL[] urls = {url};
+                    aClass = Class.forName(className, true, new java.net.URLClassLoader(urls));
+                if (PravegaSerializer.class.isAssignableFrom(aClass)) {
+                    return (PravegaSerializer) aClass.newInstance();
+                } else {
+                    throw new RuntimeException("implementation not found");
+                }
+            }
+        };
+        return doPrivileged(action);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static PravegaDeserializer getDeserializer(String className, URL url) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        final PrivilegedAction<PravegaDeserializer> action = new PrivilegedAction<PravegaDeserializer>() {
+            @Override
+            @SneakyThrows
+            @SuppressWarnings("unchecked")
+            public PravegaDeserializer run() {
+                Class<?> theClass;
+                // Load the class.
+                URL[] urls = {url};
+                theClass = Class.forName(className, true, new java.net.URLClassLoader(urls));
+                if (PravegaDeserializer.class.isAssignableFrom(theClass)) {
+                    // Create an instance of the class.
+                    return (PravegaDeserializer) theClass.newInstance();
+                } else {
+                    throw new RuntimeException("Implementation not found");
+                }
+            }
+        };
+        return doPrivileged(action);
+    }
+}
