05578eba3b44294387b9349cb17b677053e33c53
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2020-06-30 08:55:47 -0700

    resource string
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- server/src/main/java/io/pravega/schemaregistry/server/rest/resources/AuthResources.java
+++ server/src/main/java/io/pravega/schemaregistry/server/rest/resources/AuthResources.java
@@ -9,12 +9,13 @@
  */
 package io.pravega.schemaregistry.server.rest.resources;
 
+import com.google.common.base.Strings;
 import io.pravega.schemaregistry.service.Config;
 
 class AuthResources {
-    static final String DOMAIN = Config.AUTH_RESOURCE_QUALIFIER;
+    static final String DOMAIN = Strings.isNullOrEmpty(Config.AUTH_RESOURCE_QUALIFIER) ? "" : Config.AUTH_RESOURCE_QUALIFIER + "/";
     static final String DEFAULT_NAMESPACE = "";
-    static final String NAMESPACE_FORMAT = DOMAIN + "/%s";
+    static final String NAMESPACE_FORMAT = DOMAIN + "%s";
     static final String NAMESPACE_GROUP_FORMAT = NAMESPACE_FORMAT + "/%s";
     static final String NAMESPACE_GROUP_SCHEMA_FORMAT = NAMESPACE_GROUP_FORMAT + "/schemas";
     static final String NAMESPACE_GROUP_CODEC_FORMAT = NAMESPACE_GROUP_FORMAT + "/codecs";
