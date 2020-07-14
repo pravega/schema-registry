9d99d3a4445fe700476798e70685cf88c4153d02
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2020-07-14 01:16:30 -0700

    filter
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- server/src/main/java/io/pravega/schemaregistry/server/rest/filter/NamespacePathRequestFilter.java
+++ server/src/main/java/io/pravega/schemaregistry/server/rest/filter/NamespacePathRequestFilter.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
  */
 package io.pravega.schemaregistry.server.rest.filter;
 
@@ -20,41 +20,65 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 import java.util.List;
 
-@Provider
-@PreMatching
 /**
  * A request filter that convers namespace expressed as matrix parameter of path segment "groups"
  * into a query param. 
  * This allows users to express either "/v1/groups;namespace=ns" or "/v1/groups?namespace=ns". 
+ * If both path segment and query param are specified, path segment takes precedence. 
  */
+@Provider
+@PreMatching
 public class NamespacePathRequestFilter implements ContainerRequestFilter {
-	private static final String NAMESPACE = "namespace";
+    private static final String NAMESPACE = "namespace";
+    private static final String GROUPS = "groups";
 
-	@Override
-	public void filter(ContainerRequestContext containerRequest)
-			throws WebApplicationException {
-		UriInfo uriInfo = containerRequest.getUriInfo();
-		// if path has namespace defined as path segment. convert it to query param. 
-		UriBuilder requestUri = uriInfo.getRequestUriBuilder();
-		List<PathSegment> pathSegments = uriInfo.getPathSegments();
-		if (pathSegments.size() > 1 && 
-				pathSegments.get(1).getMatrixParameters().containsKey(NAMESPACE)) {
-			MultivaluedMap<String, String> matrixParams = pathSegments.get(1).getMatrixParameters();
-			String namespace = matrixParams.getFirst(NAMESPACE);
-			StringBuilder pathBuilder = new StringBuilder();
-			for (PathSegment pathSegment : pathSegments) {
-				pathBuilder.append("/");
-	
-				pathBuilder.append(pathSegment.getPath());
-				pathSegment.getMatrixParameters().forEach((x, y) -> {
-					if (!x.equals(NAMESPACE)) {
-						pathBuilder.append(";").append(x).append("=").append(y);
-					}
-				});
-			}
-			requestUri.replacePath(pathBuilder.toString());
-			requestUri.queryParam(NAMESPACE, namespace);
-		}
-		containerRequest.setRequestUri(requestUri.build());	
-	}
+    @Override
+    public void filter(ContainerRequestContext containerRequest)
+            throws WebApplicationException {
+        UriInfo uriInfo = containerRequest.getUriInfo();
+        // if path has namespace defined as path segment. convert it to query param. 
+        UriBuilder uriBuilder = uriInfo.getRequestUriBuilder();
+        List<PathSegment> pathSegments = uriInfo.getPathSegments();
+        if (pathSegments.size() > 1 && pathSegments.get(1).getPath().equals(GROUPS) && 
+                pathSegments.get(1).getMatrixParameters().containsKey(NAMESPACE)) {
+            handlePathSegment(uriBuilder, pathSegments);
+        } else if (pathSegments.size() > 2 && pathSegments.get(1).getPath().equals(NAMESPACE)) {
+            handleNamespacePath(uriBuilder, pathSegments);
+        }
+        containerRequest.setRequestUri(uriBuilder.build());
+    }
+
+    private void handleNamespacePath(UriBuilder uriBuilder, List<PathSegment> pathSegments) {
+        StringBuilder pathBuilder = new StringBuilder();
+        pathBuilder.append("/").append(pathSegments.get(0).getPath());
+        String namespace = pathSegments.get(2).getPath();
+        for (int i = 3; i < pathSegments.size(); i++) {
+            pathBuilder.append("/");
+            PathSegment pathSegment = pathSegments.get(i);
+            pathBuilder.append(pathSegment.getPath());
+        }
+
+        uriBuilder.replacePath(pathBuilder.toString());
+        uriBuilder.replaceQueryParam(NAMESPACE, namespace);
+    }
+
+    private void handlePathSegment(UriBuilder uriBuilder, List<PathSegment> pathSegments) {
+        // replace the path segment for groups (at index 1) with
+        MultivaluedMap<String, String> matrixParams = pathSegments.get(1).getMatrixParameters();
+        String namespace = matrixParams.getFirst(NAMESPACE);
+        StringBuilder pathBuilder = new StringBuilder();
+        for (int i = 0; i < pathSegments.size(); i++) {
+            PathSegment pathSegment = pathSegments.get(i);
+            pathBuilder.append("/");
+            pathBuilder.append(pathSegment.getPath());
+            final int index = i;
+            pathSegment.getMatrixParameters().forEach((x, y) -> {
+                if (index != 1 || !x.equals(NAMESPACE)) {
+                    pathBuilder.append(";").append(x).append("=").append(y);
+                }
+            });
+        }
+        uriBuilder.replacePath(pathBuilder.toString());
+        uriBuilder.replaceQueryParam(NAMESPACE, namespace);
+    }
 }
