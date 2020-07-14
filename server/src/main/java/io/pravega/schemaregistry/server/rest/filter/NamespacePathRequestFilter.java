1acd02513b7b28782a1f8db8ae3991b3a89a6219
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2020-07-14 04:45:06 -0700

    namespace filter
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- server/src/main/java/io/pravega/schemaregistry/server/rest/filter/NamespacePathRequestFilter.java
+++ server/src/main/java/io/pravega/schemaregistry/server/rest/filter/NamespacePathRequestFilter.java
@@ -13,7 +13,6 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.PathSegment;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
@@ -21,22 +20,18 @@ import javax.ws.rs.ext.Provider;
 import java.util.List;
 
 /**
- * A request filter that converts namespace expressed either as a path param or as matrix parameter of path segment "groups"
- * into a query param. 
+ * A request filter that converts namespace expressed as a path param into a query param. 
  * 
- * This allows users to express either "/v1/namespace/ns/groups" or "/v1/groups;namespace=ns" or "/v1/groups?namespace=ns". 
+ * This allows users to express either "/v1/namespace/ns/groups" "/v1/groups?namespace=ns" and both are equivalent. 
  * 
- * Just as with query params, if more than one query param is specified with same name, the first one takes precedence. 
- * Similarly, if more than one namespace value is provided, then the preference order is path param followed by path segment 
- * followed by query param.
- * So if someone hits the uri "/v1/namespace/ns1/groups;namespace=ns2?namespace=ns3". This will result in namespace being 
+ * If both path param and query param is used for namespace value is provided, then the path param overrides the query param. 
+ * So if someone expresses the uri as "/v1/namespace/ns1/groups?namespace=ns2". This will result in namespace being 
  * set to ns1 for the request.  
  */
 @Provider
 @PreMatching
 public class NamespacePathRequestFilter implements ContainerRequestFilter {
     private static final String NAMESPACE = "namespace";
-    private static final String GROUPS = "groups";
 
     @Override
     public void filter(ContainerRequestContext containerRequest)
@@ -45,10 +40,7 @@ public class NamespacePathRequestFilter implements ContainerRequestFilter {
         // if path has namespace defined as path segment. convert it to query param. 
         UriBuilder uriBuilder = uriInfo.getRequestUriBuilder();
         List<PathSegment> pathSegments = uriInfo.getPathSegments();
-        if (pathSegments.size() > 1 && pathSegments.get(1).getPath().equals(GROUPS) && 
-                pathSegments.get(1).getMatrixParameters().containsKey(NAMESPACE)) {
-            handlePathSegment(uriBuilder, pathSegments);
-        } else if (pathSegments.size() > 2 && pathSegments.get(1).getPath().equals(NAMESPACE)) {
+        if (pathSegments.size() > 2 && pathSegments.get(1).getPath().equals(NAMESPACE)) {
             handleNamespacePath(uriBuilder, pathSegments);
         }
         containerRequest.setRequestUri(uriBuilder.build());
@@ -71,12 +63,5 @@ public class NamespacePathRequestFilter implements ContainerRequestFilter {
     private void appendPath(PathSegment pathSegment, StringBuilder pathBuilder) {
         pathBuilder.append("/").append(pathSegment.getPath());
         pathSegment.getMatrixParameters().forEach((x, y) -> pathBuilder.append(";").append(x).append("=").append(y));
-    }
-
-    private void handlePathSegment(UriBuilder uriBuilder, List<PathSegment> pathSegments) {
-        // replace the path segment for groups (at index 1) with
-        MultivaluedMap<String, String> matrixParams = pathSegments.get(1).getMatrixParameters();
-        String namespace = matrixParams.getFirst(NAMESPACE);
-        uriBuilder.replaceQueryParam(NAMESPACE, namespace);
     }
 }
