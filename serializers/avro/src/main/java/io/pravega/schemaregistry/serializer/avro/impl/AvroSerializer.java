b465ee09e8d66292212782962188846d51a424b4
jingerbread <qwadrillion@gmail.com>
2020-10-01 18:59:26 +0300

    [fixed] Issue 140 Move creation of DatumWriters(thread safe) in AvroSerializer class to constructor

--- serializers/avro/src/main/java/io/pravega/schemaregistry/serializer/avro/impl/AvroSerializer.java
+++ serializers/avro/src/main/java/io/pravega/schemaregistry/serializer/avro/impl/AvroSerializer.java
@@ -14,7 +14,6 @@ import io.pravega.schemaregistry.client.SchemaRegistryClient;
 import io.pravega.schemaregistry.serializer.shared.codec.Encoder;
 import io.pravega.schemaregistry.contract.data.SchemaInfo;
 import io.pravega.schemaregistry.serializer.shared.impl.AbstractSerializer;
-import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.IndexedRecord;
 import org.apache.avro.io.BinaryEncoder;
@@ -27,30 +26,32 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 public class AvroSerializer<T> extends AbstractSerializer<T> {
-    private final AvroSchema<T> avroSchema;
+    private final org.apache.avro.Schema avroSchema;
+    private final SpecificDatumWriter<T> specificDatumWriter;
+    private final GenericDatumWriter<T> genericDatumWriter;
+    private final ReflectDatumWriter<T> reflectDatumWriter;
+
     public AvroSerializer(String groupId, SchemaRegistryClient client, AvroSchema<T> schema,
                    Encoder encoder, boolean registerSchema) {
         super(groupId, client, schema, encoder, registerSchema, true);
-        this.avroSchema = schema;
+        this.avroSchema = schema.getSchema();
+        this.specificDatumWriter = new SpecificDatumWriter<>(avroSchema);
+        this.genericDatumWriter = new GenericDatumWriter<>(avroSchema);
+        this.reflectDatumWriter = new ReflectDatumWriter<>(avroSchema);
     }
 
     @Override
     protected void serialize(T var, SchemaInfo schemaInfo, OutputStream outputStream) throws IOException {
-        Schema schema = avroSchema.getSchema();
-        
         BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
 
         if (IndexedRecord.class.isAssignableFrom(var.getClass())) {
             if (SpecificRecord.class.isAssignableFrom(var.getClass())) {
-                SpecificDatumWriter<T> writer = new SpecificDatumWriter<>(schema);
-                writer.write(var, encoder);
+                specificDatumWriter.write(var, encoder);
             } else {
-                GenericDatumWriter<T> writer = new GenericDatumWriter<>(schema);
-                writer.write(var, encoder);
+                genericDatumWriter.write(var, encoder);
             }
         } else {
-            ReflectDatumWriter<T> writer = new ReflectDatumWriter<>(schema);
-            writer.write(var, encoder);
+            reflectDatumWriter.write(var, encoder);
         }
 
         encoder.flush();
